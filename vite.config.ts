a12b82a53622540503c1f5d8e703f155b6aff828
shosingh <shobhit.singh@zalando.de>
2025-02-19 04:41:30 +0100

    34

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,5 @@ export default defineConfig({
       '@shared': path.resolve(__dirname, 'shared'),
     },
   },
-  root: path.resolve(__dirname, 'client'),
+  root: path.resolve(__dirname, '../WhatsAppHousingHub'),
 });
