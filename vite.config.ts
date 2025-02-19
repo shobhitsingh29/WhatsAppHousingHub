d5a8ec45dfd93dbfb4ffee095e6ef2cbd3e1679f
shosingh <shobhit.singh@zalando.de>
2025-02-19 04:30:49 +0100

    34

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import themePlugin from '@replit/vite-plugin-shadcn-theme-json';
 import path, { dirname } from 'path';
 import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal';
 import { fileURLToPath } from 'url';
