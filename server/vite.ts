6b5fdff8188a5ef842f227cce07580543d73c2f2
shosingh <shobhit.singh@zalando.de>
2025-02-19 04:26:51 +0100

    34

--- server/vite.ts
+++ server/vite.ts
@@ -79,7 +79,6 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
   app.use('*', (_req, res) => {
