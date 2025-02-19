ffaad1058414f029d72c7bb11128a24e3bad701b
shosingh <shobhit.singh@zalando.de>
2025-02-19 03:23:22 +0100

    db-live

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import themePlugin from '@replit/vite-plugin-shadcn-theme-json';
 import path, { dirname } from 'path';
-import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client', 'src'),
@@ -17,7 +16,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, 'client'),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, 'dist/public'),
     emptyOutDir: true,
   },
 });
